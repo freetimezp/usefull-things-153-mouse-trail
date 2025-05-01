03b3165fa3febd14cb09848654f605999bfd5ff8
freetimezp <kraftukrnet@ukr.net>
2025-05-01 23:27:35 +0300

    02. usefull-things-153-mouse-trail. UI. Mousemove animation.

---
+++ assets/js/main.js
@@ -0,0 +1,57 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+    const container = document.querySelector(".items");
+    let imageIndex = 1;
+    let animationTimeout = null;
+    let currentlyPlaying = false;
+
+    function addNewItem(x, y) {
+        const newItem = document.createElement("div");
+        newItem.className = "item";
+        newItem.style.left = `${x - 75}px`;
+        newItem.style.top = `${y - 100}px`;
+
+        const img = document.createElement("img");
+        img.src = `./assets/images/${imageIndex}.jpg`;
+        newItem.appendChild(img);
+        imageIndex = (imageIndex % 15) + 1;
+
+        container.appendChild(newItem);
+        manageItemLimit();
+    }
+
+    function manageItemLimit() {
+        while (container.children.length > 20) {
+            container.removeChild(container.firstChild);
+        }
+    }
+
+    function startAnimation() {
+        if (currentlyPlaying || container.children.length === 0) return;
+        currentlyPlaying = true;
+
+        gsap.to(".item", {
+            y: 1000,
+            scale: 0.5,
+            opacity: 0,
+            duration: 0.5,
+            stagger: 0.05,
+            onComplete: function () {
+                this.targets().forEach((item) => {
+                    if (item.parentNode) {
+                        item.parentNode.removeChild(item);
+                    }
+                });
+
+                currentlyPlaying = false;
+            }
+        });
+    }
+
+    container.addEventListener("mousemove", function (event) {
+        clearTimeout(animationTimeout);
+        addNewItem(event.pageX, event.pageY);
+
+        animationTimeout = setTimeout(startAnimation, 500);
+    });
+});
